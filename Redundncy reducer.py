4da4e6f14816c555d69a2ddc54bf7a1ed6756603
kunalverma01 <37834438+kunalverma01@users.noreply.github.com>
2018-12-11 12:16:35 +0530

    hellow

--- Redundncy reducer.py
+++ Redundncy reducer.py
@@ -1,14 +1,1 @@
-def Remove(duplicate): 
-    final_list = [] 
-    for num in duplicate: 
-        if num not in final_list: 
-            final_list.append(num) 
-    return final_list 
-      
-
-    
-
-if __name__=="__main__":
-    duplicate = [1,3,4,1,3,4,1,3,4]
-    duplicate.sort()
-    print(Remove(duplicate)) 
+print(Hello Git!)
